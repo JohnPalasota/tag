39f45559e329e0b7c42459be922cdaaa40c823e7
John Palasota <John.Palasota@improving.com>
2019-09-19 10:21:45 -0500

    removing constructors from game

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start of Main");
-        Game game = new Game(   );
+        Game game = new Game();
         System.out.println("Declared Game");
-            game.run();
+        game.run();
         System.out.println("After run()");
 
         long elapsedTicks = game.getEndTime().getTime() - game.getStartTime().getTime();
