8d7c5744cb8786dc4d24924ca89249820b556671
John Palasota <John.Palasota@improving.com>
2019-09-22 14:53:02 -0500

    Added tests for inventory, jump, look, and teleport

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context =
+                AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(SpringContext.class);
 
 
---
+++ src/test/java/LookCommandTests.java
@@ -0,0 +1,74 @@
+import org.improving.tag.commands.InventoryCommand;
+import org.improving.tag.commands.LookCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LookCommandTests {
+    LookCommand target;
+    TestInputOutput io;
+
+    @BeforeEach //Before you run each test, run this upcoming method.
+    public void arrange(){
+        // Arrange
+        io = new TestInputOutput();
+        target = new LookCommand(io);
+    }
+    @Test
+    public void execute_should_return_phrase(){
+        //Act
+        target.execute(null);
+
+        //Assert
+        assertEquals("You look around.", io.lastText);
+    }
+
+    @Test
+    public void isValid_should_be_true_when_input_is_look(){
+        //Act
+        var result = target.isValid( "look");
+
+        //Assert
+        assertTrue(result);
+
+    }
+
+    @Test
+    public void isValid_should_be_false_when_input_is_foobar(){
+        //Act
+        var result = target.isValid( "foobar");
+
+        //Assert
+        assertFalse(result);
+    }
+    @Test
+    public void isValid_should_be_false_when_input_is_null(){
+        //Act
+        var result = target.isValid( null);
+
+        //Assert
+        assertFalse(result);
+    }
+    @Test
+    public void isValid_should_be_true_when_input_is_look_with_spaces(){
+        //Act
+        var result = target.isValid( "    look ");
+
+        //Assert
+        assertTrue(result);
+
+    }
+
+    @Test
+    public void isValid_should_be_true_when_input_is_look_with_caps(){
+        //Act
+        var result = target.isValid( "LOOk");
+
+        //Assert
+        assertTrue(result);
+
+    }
+
+}
